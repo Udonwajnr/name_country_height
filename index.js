24b4354fb80ab003f17d1b9f81712a2f81c76a96
udonwajnr <udonwajnr10@gmail.com>
2022-06-21 13:53:17 +0100

    zuri

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const name = 'Umoh Udonwa'
 const country = 'Nigeria'
 const height = '5.9 ft'
 
-alert(`My name is ${name}. I'm from ${country}. I'm ${height} in height`)
+alert(`My name is ${name}. I'm from ${country}. I'm ${height} in height`) 
